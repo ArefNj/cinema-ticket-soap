916e9ee35679f5dfdf8e2e627344fc6718d2c5bc
aref <aref.najmaddini@gmail.com>
2026-05-27 19:48:12 +0330

    Initial commit

---
+++ src/main/java/org/soap/cinema/cinematicketsoap/CinemaTicketSoapApplication.java
@@ -0,0 +1,13 @@
+package org.soap.cinema.cinematicketsoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaTicketSoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaTicketSoapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/soap/cinema/cinematicketsoap/CinemaTicketSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package org.soap.cinema.cinematicketsoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaTicketSoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
