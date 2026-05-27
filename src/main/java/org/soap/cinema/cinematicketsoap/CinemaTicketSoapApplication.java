6cbcff1d14f4428e97d9470376992c68ce5feba0
aref <aref.najmaddini@gmail.com>
2026-05-27 19:54:03 +0330

    Add dependencies for SOAP web service support
    Integrates Apache CXF, WSDL4J, and Jakarta XML Binding to enable the development and consumption of SOAP web services. This provides the core framework for the `cinema-ticket-soap` application.
    
    Updates `.gitignore` to exclude `*.pdf` files, anticipating generated documentation or artifacts related to the SOAP services.

--- src/main/java/org/soap/cinema/cinematicketsoap/CinemaTicketSoapApplication.java
+++ src/main/java/org/soap/cinema/cinematicketsoap/CinemaTicketSoapApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CinemaTicketSoapApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CinemaTicketSoapApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaTicketSoapApplication.class, args);
+    }
 
 }
